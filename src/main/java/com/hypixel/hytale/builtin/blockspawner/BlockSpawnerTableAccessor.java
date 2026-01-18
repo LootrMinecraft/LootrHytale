a077c917d01b50cb905b2249f4f9ec4da7d14781
Jon <due@wxwhatever.com>
2026-01-18 13:44:44 +1000

    Notes.

--- src/main/java/com/hypixel/hytale/builtin/blockspawner/BlockSpawnerTableAccessor.java
+++ src/main/java/com/hypixel/hytale/builtin/blockspawner/BlockSpawnerTableAccessor.java
@@ -2,6 +2,8 @@ package com.hypixel.hytale.builtin.blockspawner;
 
 import com.hypixel.hytale.common.map.IWeightedMap;
 
+// Split package accessors = extra cursed
+// TODO: Just switch to VarHandle or Hyxin
 public class BlockSpawnerTableAccessor {
   public static void setEntries (BlockSpawnerTable table, IWeightedMap<BlockSpawnerEntry> entries) {
     table.entries = entries;
